fe0406968df870a2eaa605fd556a1005c1ea1f60
Epicpoop21 <l9w1sph@gmail.com>
2022-07-09 22:34:27 +0100

    Added some improvements

--- PlayerMovement.cs
+++ PlayerMovement.cs
@@ -4,16 +4,48 @@ public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] private float speed;
     private Rigidbody2D body;
+    private Animator anim;
 
     private void Awake()
     {
+        //Grab referances for rigidbody and animator from objects
         body = GetComponent<Rigidbody2D>(); 
+        anim = GetComponent<Animator>();
     }
     private void Update()
     {
-        body.velocity = new Vector2(Input.GetAxis("Horizontal") * speed, body.velocity.y);
+        float horizontalInput = Input.GetAxis("Horizontal");
+        body.velocity = new Vector2(horizontalInput * speed, body.velocity.y);
 
-        if(Input.GetKey(KeyCode.Space))
-            body.velocity = new Vector2(body.velocity.x, speed);
+        //Flips player when moving left-right
+        if (horizontalInput > 0.01f)
+            transform.localScale = Vector3.one;
+        else if (horizontalInput < -0.01f)
+            transform.localScale = new Vector3(-1, 1, 1);
+
+        if (Input.GetKey(KeyCode.Space) && isGrounded)
+          Jump();
+
+        //Sets animator params
+        anim.SetBool("run", horizontalInput != 0);
+        anim.SetBool("grounded", isGrounded);
+    }
+
+    private void Jump()
+    {
+        body.velocity = new Vector2(body.velocity.x, speed);
+        anim.SetTrigger("jump");
+        isGrounded = false;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision) 
+    {
+        if (collision.gameObject.tag == "Ground")
+            isGrounded = true;
+    }
+
+    private bool isisGrounded()
+    {
+        return false;
     }
 }
